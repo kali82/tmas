7ca69a03fdd19d53ddc0222c31f6e83364b0f904
mkalisz_96 <kaliszewskimaciej@gmail.com>
2022-11-05 13:54:43 +0100

    map update

--- js/initMap.js
+++ js/initMap.js
@@ -1,14 +1,37 @@
         //Defining map as a global variable to access from other functions
         var map;
         var polyline;
- 
+        let germanyPolyline;
+        let cechPolyline;
+        let slovakPolyline;
+
+
         var lineCoordinates = [
            [44.0715858,27.2454436],[43.83531,25.9752809],
 		   [43.0840727,25.6331224],[42.6954322,23.3239467],
 		   [52.3747157,4.8986167],[51.2384547,6.8143503]
         ];
+        let germanyCoordinates = [
+            [52.44298956333314, 18.720196949533225], [51.477107, 9.851322],
+            [51.477107, 9.851322], [52.44298956333314, 18.720196949533225] 
+        ];
+        let cechCoordinates = [
+            [52.44298956333314, 18.720196949533225], [49.81764566235488, 15.205056094046585]
+        ];
+        let slovakCoordinates = [
+            [52.44298956333314, 18.720196949533225], [49.09633422549106, 18.651814125006133]
+        ];
  
         function addAnimatedPolyline () {
+            // Defining truck  symbol
+            var arrowSymbol = {
+                rotation: 90,
+                strokeWeight: 0.5,
+                strokeColor: 'rgb(25, 0, 255)',
+                scale: (-0.01,0.1),
+                path: 'M495.99,121.797H171.298c-8.842,0-16.01,7.168-16.01,16.01v157.57h-10.332V140.375c0-8.842-7.168-16.01-16.01-16.01H95.579c-29.375,0-53.748,21.813-57.778,50.091C15.716,182.941,0,204.375,0,229.414v117.53c0,8.842,7.168,16.01,16.01,16.01H40.93c9.019,16.234,26.34,27.249,46.192,27.249c20.144,0,37.685-11.341,46.588-27.97c6.516-2.028,11.248-8.107,11.248-15.29v-19.546h10.332v19.546c0,8.842,7.168,16.01,16.01,16.01h44.496c9.019,16.234,26.34,27.249,46.192,27.249s37.173-11.015,46.192-27.249h65.686c9.019,16.234,26.34,27.249,46.192,27.249s37.172-11.015,46.192-27.249h29.744c8.842,0,16.01-7.168,16.01-16.01V137.808C512,128.966,504.833,121.797,495.99,121.797z M87.121,358.182c-11.464,0-20.79-9.326-20.79-20.79s9.327-20.79,20.79-20.79s20.791,9.327,20.791,20.79S98.586,358.182,87.121,358.182z M112.936,291.348c-7.639-4.299-16.443-6.765-25.815-6.765c-26.931,0-49.203,20.269-52.4,46.351h-2.7V260.42c1.515,0.475,3.125,0.733,4.797,0.733h24.711c8.842,0,16.01-7.168,16.01-16.01s-7.168-16.01-16.01-16.01H36.817c-1.671,0-3.282,0.258-4.797,0.733v-0.453c0-14.792,12.034-26.828,26.828-26.828h54.088V291.348z M112.936,170.566H72.217c4.4-8.42,13.221-14.181,23.362-14.181h17.357V170.566z M187.308,153.818h292.671v16.748H187.308V153.818z M261.985,358.183c-11.464,0-20.79-9.327-20.79-20.79s9.325-20.79,20.79-20.79s20.791,9.325,20.791,20.79C282.776,348.857,273.45,358.183,261.985,358.183z M420.054,358.183c-11.464,0-20.791-9.327-20.791-20.79s9.326-20.79,20.791-20.79s20.79,9.325,20.79,20.79C440.844,348.857,431.519,358.183,420.054,358.183z M479.98,330.934h-7.524v-0.001c-3.198-26.082-25.469-46.351-52.4-46.351c-26.931,0-49.204,20.269-52.402,46.351h-53.267c-3.198-26.082-25.47-46.351-52.401-46.351s-49.203,20.269-52.4,46.351h-22.277V202.586H479.98V330.934z'
+            };
+
             //First we iterate over the coordinates array to create a
             // new array which includes objects of LatLng class.
             var pointCount = lineCoordinates.length;
@@ -17,15 +40,61 @@
                 var tempLatLng = new google.maps.LatLng(lineCoordinates[i][0], lineCoordinates[i][1]);
                 linePath.push(tempLatLng);
             }
- 
-            // Defining arrow symbol
-            var arrowSymbol = {
-                rotation: 270,
-                strokeWeight: 4,
-                strokeColor: '#080082',
-                scale: 0.1,
-                path: 'M21.474,377.522V117.138c0-14.469,11.729-26.199,26.199-26.199h260.25c14.469,0,26.198,11.73,26.198,26.199v260.385   c0,4.823-3.909,8.733-8.733,8.733H30.207C25.383,386.256,21.474,382.346,21.474,377.522z M231.634,466.724   c0,30.01-24.329,54.338-54.338,54.338c-30.009,0-54.338-24.328-54.338-54.338c0-30.011,24.329-54.338,54.338-54.338   C207.305,412.386,231.634,436.713,231.634,466.724z M204.464,466.724c0-15.005-12.164-27.169-27.169-27.169   s-27.17,12.164-27.17,27.169s12.165,27.17,27.17,27.17S204.464,481.729,204.464,466.724z M130.495,412.385H8.733   c-4.823,0-8.733,3.91-8.733,8.733v26.495c0,4.823,3.91,8.733,8.733,8.733h97.598C108.879,438.862,117.704,423.418,130.495,412.385z    M515.938,466.724c0,30.01-24.329,54.338-54.338,54.338c-30.01,0-54.338-24.328-54.338-54.338   c0-30.011,24.328-54.338,54.338-54.338C491.609,412.385,515.938,436.713,515.938,466.724z M488.77,466.724   c0-15.005-12.165-27.169-27.17-27.169c-15.006,0-27.169,12.164-27.169,27.169s12.164,27.17,27.169,27.17   S488.77,481.729,488.77,466.724z M612,421.118v26.495c0,4.823-3.91,8.733-8.733,8.733h-70.704   c-5.057-34.683-34.906-61.427-70.961-61.427c-36.062,0-65.912,26.745-70.969,61.427H248.261   c-2.549-17.483-11.373-32.928-24.164-43.961h134.994V162.594c0-9.646,7.82-17.466,17.466-17.466h82.445   c23.214,0,44.911,11.531,57.9,30.77l53.15,78.721c7.796,11.547,11.962,25.161,11.962,39.094v118.672h21.253   C608.09,412.385,612,416.295,612,421.118z M523.408,256.635l-42.501-60.393c-1.636-2.324-4.3-3.707-7.142-3.707H407.47   c-4.822,0-8.733,3.91-8.733,8.733v60.393c0,4.824,3.91,8.733,8.733,8.733h108.798C523.342,270.394,527.48,262.421,523.408,256.635z'
-            };
+
+            ///GERMANY
+            var germanyCount = germanyCoordinates.length;
+            var germanyPath = [];
+            for (var i=0; i < germanyCount; i++) {
+                var tempLatLng = new google.maps.LatLng(germanyCoordinates[i][0], germanyCoordinates[i][1]);
+                germanyPath.push(tempLatLng);
+            }
+
+            var germanyLineOptions = {
+                path: germanyPath,
+                icons: [{
+                    icon: arrowSymbol
+                    //offset: '100%'
+                }],
+                strokeWeight: 2,
+                strokeColor: '#2D9745',
+                strokeOpacity: 1
+            }
+                        ///CECHIA
+                        var cechCount = cechCoordinates.length;
+                        var cechPath = [];
+                        for (var i=0; i < cechCount; i++) {
+                            var tempLatLng = new google.maps.LatLng(cechCoordinates[i][0], cechCoordinates[i][1]);
+                            cechPath.push(tempLatLng);
+                        }
+            
+                        var cechLineOptions = {
+                            path: cechPath,
+                            icons: [{
+                                icon: arrowSymbol
+                                //offset: '100%'
+                            }],
+                            strokeWeight: 2,
+                            strokeColor: '#2D9745',
+                            strokeOpacity: 1
+                        }
+            ///SLOVAKIA
+            var slovakCount = slovakCoordinates.length;
+            var slovakPath = [];
+            for (var i=0; i < slovakCount; i++) {
+                var tempLatLng = new google.maps.LatLng(slovakCoordinates[i][0], slovakCoordinates[i][1]);
+                slovakPath.push(tempLatLng);
+            }
+
+            var slovakLineOptions = {
+                path: slovakPath,
+                icons: [{
+                    icon: arrowSymbol
+                    //offset: '100%'
+                }],
+                strokeWeight: 2,
+                strokeColor: '#2D9745',
+                strokeOpacity: 1
+            } 
  
             // Polyline properties are defined below
             var lineOptions = {
@@ -34,20 +103,31 @@
                     icon: arrowSymbol
                     //offset: '100%'
                 }],
-                strokeWeight: 3,
-                strokeColor: '#FF0000',
+                strokeWeight: 2,
+                strokeColor: '#2D9745',
                 strokeOpacity: 1
             }
-            polyline = new google.maps.Polyline(lineOptions);
- 
+            //polyline = new google.maps.Polyline(lineOptions);
+            germanyPolyline = new google.maps.Polyline(germanyLineOptions);
+            cechPolyline = new google.maps.Polyline(cechLineOptions);
+            slovakPolyline = new google.maps.Polyline(slovakLineOptions);
+
             // Polyline is set to current map
-            polyline.setMap(map);
+            //polyline.setMap(map);
+            germanyPolyline.setMap(map)
+            cechPolyline.setMap(map)
+            slovakPolyline.setMap(map)
+
  
             // Calling the arrow animation function
-            animateArrow();
+           // animateArrow(polyline);
+            animateArrow(germanyPolyline);
+            animateArrow(cechPolyline);
+            animateArrow(slovakPolyline);
+
         }
  
-        function animateArrow() {
+        function animateArrow(polyline) {
             var counter = 0;
             var accessVar = window.setInterval(function() {
                 counter = (counter + 1) % 350;
@@ -55,7 +135,7 @@
                 var arrows = polyline.get('icons');
                 arrows[0].offset = (counter / 2) + '%';
                 polyline.set('icons', arrows);
-            }, 50);
+            },20);
         }
  
         function initMap() {
@@ -65,7 +145,7 @@
             //Setting starting options of map
             var mapOptions = {
                 center: new google.maps.LatLng(47.5550609,18.5756966),
-                zoom: 6,
+                zoom: 5,
                 // mapTypeId: google.maps.MapTypeId.ROADMAP
             };
  
