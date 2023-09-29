430d281bfd5bda429edbf659052a03b992cedb4e
kaliszewskimaciej <maciej.kaliszewski@ttpsc.pl>
2023-09-29 13:21:17 +0200

    deploy

--- js/initMap.js
+++ js/initMap.js
@@ -1,19 +1,15 @@
-
-
-
-
- var map2;
+var map2;
 function contactMap() {
-    const officeLocation = { lat: 51.96170736757314, lng: 15.535628188855894 };
-    const map2 = new google.maps.Map(document.getElementById("contact-map"), {
-      zoom: 16,
-      center: officeLocation,
-    });
-    map2.setMapTypeId(google.maps.MapTypeId.HYBRID)
-    const marker = new google.maps.Marker({
-      position: officeLocation,
-      map: map2,
-    });
-  }
-  
+  const officeLocation = { lat: 51.96170736757314, lng: 15.535628188855894 };
+  const map2 = new google.maps.Map(document.getElementById("contact-map"), {
+    zoom: 16,
+    center: officeLocation,
+  });
+  map2.setMapTypeId(google.maps.MapTypeId.HYBRID);
+  const marker = new google.maps.Marker({
+    position: officeLocation,
+    map: map2,
+  });
+}
+
 window.initMap = map2;
